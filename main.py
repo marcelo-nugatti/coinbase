a73aeb7063a54cac33b15a7857a391f25e3bf2b4
Marcelo Nuñez <real.nugatti@gmail.com>
2020-03-12 10:40:01 -0500

    new commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import rumps
+
+class AwesomeStatusBarApp(rumps.App):
+    def __init__(self):
+        super(AwesomeStatusBarApp, self).__init__("BettaTech")
+        self.menu = ["Queryng API..."]
+
+    @rumps.timer(1)
+    def sayhi(self, _):
+        headers = {
+                'Content-Type': 'application/json',
+                }
+        r = requests.get("htttps://api.coinbase.com/v2/exchange-rates?currency=BTC")
+        print(r.json()['data']['rates']['EUR'])
+        self.title = r.json()['data']['rates']['EUR']
+        
+if __name__ == "__main__":
+    AwesomeStatusBarApp().run()
